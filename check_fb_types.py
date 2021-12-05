fb434abe20a6f9573cbeab6ad6a798f77ae5ab05
Morten Terhart <32468043+mortenterhart@users.noreply.github.com>
2021-12-06 00:12:57 +0100

    Check how many types are available for Freebase in freebaseTypes.tsv

---
+++ check_fb_types.py
@@ -0,0 +1,10 @@
+import pandas as pd
+
+if __name__ == '__main__':
+    fb = pd.read_pickle('dataset/FB15K237/dataset.pkl')
+    fb_types = pd.read_csv('dataset/freebase/freebaseTypes.tsv', sep='\t', names=['S', 'Class'])
+    fb_types = fb_types.drop_duplicates(['S'])
+
+    typed_subjects = pd.merge(fb, fb_types, on='S', how='inner')
+
+    print(f'Found types for {len(typed_subjects)} subjects out of {len(fb)} ({len(typed_subjects)/len(fb)*100:.2f}%)')
