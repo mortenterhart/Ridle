458e42fe7289c47acd1104c75643ac3fd512f1e2
Morten Terhart <32468043+mortenterhart@users.noreply.github.com>
2022-01-12 00:48:52 +0100

    Revert type check back to Freebase dataset

--- check_fb_types.py
+++ check_fb_types.py
@@ -1,10 +1,9 @@
 import pandas as pd
 
 if __name__ == '__main__':
-    fb = pd.read_pickle('dataset/YAGO3-10/dataset.pkl')
-    fb_types = pd.read_csv('dataset/yago/SimpleTypeFactsWordnetLevel.tsv', sep='\t', names=['S', 'P', 'Class'])
+    fb = pd.read_pickle('dataset/FB15K237/dataset.pkl').drop_duplicates(['S'])
+    fb_types = pd.read_csv('dataset/freebase/freebaseTypes.tsv', sep='\t', names=['S', 'Class'])
     fb_types = fb_types.drop_duplicates(['S'])
-    fb_types = fb_types.replace(['^<', '>$'], ['', ''], regex=True)
 
     typed_subjects = pd.merge(fb, fb_types, on='S', how='inner')
 
