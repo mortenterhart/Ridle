095b0bb0a7919dc02ed850d243431579eeb590cc
Morten Terhart <32468043+mortenterhart@users.noreply.github.com>
2022-01-19 11:31:31 +0100

    Extract FB YAGO subsets with new classes and exclude classes with members less than 40

--- divide_datasets.py
+++ divide_datasets.py
@@ -1,9 +1,18 @@
 import pandas as pd
 import os
 
+from fb_yago_subsets import fb_yago_subsets
+from dataset_class_counts import dataset_class_counts
+
+MIN_CLASS_MEMBERS = 40
+
 
 def extract_dataset(df, labels):
-    return pd.concat([df[df['Class'].str.contains('wordnet_' + label + '_[0-9]', regex=True)] for label in labels])
+    class_counts = dataset_class_counts(df[['S', 'Class']].drop_duplicates())
+    labels = set(class_counts.index) & set(labels)
+    class_counts = class_counts[list(labels)]
+    include_types = class_counts[class_counts >= MIN_CLASS_MEMBERS].index
+    return pd.concat([df[df['Class'] == label] for label in include_types]).reset_index(drop=True)
 
 
 def main():
@@ -16,37 +25,18 @@ def main():
     yago_types = yago_types[['S', 'Class']].replace(['^<', '>$'], '', regex=True)
     yago_labelled = yago_triples.merge(yago_types, on='S', how='inner')
 
-    # Extract datasets
-    fb_level_1 = extract_dataset(fb_labelled, ['person', 'organization', 'body_of_water', 'product'])
-    fb_level_2_org = extract_dataset(fb_labelled, ['institution', 'musical_organization', 'party', 'enterprise', 'nongovernmental_organization'])
-    fb_level_2_person = extract_dataset(fb_labelled, ['artist', 'politician', 'scientist', 'officeholder', 'writer'])
-    fb_level_3_person_writer = extract_dataset(fb_labelled, ['journalist', 'poet', 'novelist', 'scriptwriter', 'dramatist',
-                                                             'essayist', 'biographer'])
+    for dataset_name, include_types in fb_yago_subsets.items():
+        print(f'Extracting dataset {dataset_name}')
 
-    yago_level_1 = extract_dataset(yago_labelled, ['person', 'organization', 'body_of_water', 'product'])
-    yago_level_2_org = extract_dataset(yago_labelled, ['institution', 'musical_organization', 'party', 'enterprise', 'nongovernmental_organization'])
-    yago_level_2_body_of_water = extract_dataset(yago_labelled, ['stream', 'lake', 'ocean', 'bay', 'sea'])
-    yago_level_2_person = extract_dataset(yago_labelled, ['artist', 'politician', 'scientist', 'officeholder', 'writer'])
-    yago_level_3_person_writer = extract_dataset(yago_labelled, ['journalist', 'poet', 'novelist', 'scriptwriter', 'dramatist',
-                                                                 'essayist', 'biographer'])
-    yago_level_3_person_artist = extract_dataset(yago_labelled, ['painter', 'sculptor', 'photographer', 'illustrator', 'printmaker'])
-    yago_level_3_person_player = extract_dataset(yago_labelled, ['hockey_player', 'soccer_player', 'ballplayer', 'volleyball_player', 'golfer'])
-    yago_level_3_person_scientist = extract_dataset(yago_labelled, ['social_scientist', 'biologist', 'physicist',
-                                                                    'mathematician', 'chemist', 'linguist', 'psychologist', 'geologist', 'computer_scientist', 'research_worker'])
+        dataset = fb_labelled if 'fb' in dataset_name.lower() else yago_labelled
+        subset = extract_dataset(dataset, include_types)
 
-    # Save datasets in pickle
-    dataset_names = ['FB-L1', 'FB-L2-org', 'FB-L2-person', 'FB-L3-person-writer', 'YAGO-L1', 'YAGO-L2-org',
-                     'YAGO-L2-body_of_water', 'YAGO-L2-person', 'YAGO-L3-person-writer', 'YAGO-L3-person-artist',
-                     'YAGO-L3-person-player', 'YAGO-L3-person-scientist']
-    datasets = [fb_level_1, fb_level_2_org, fb_level_2_person, fb_level_3_person_writer, yago_level_1, yago_level_2_org,
-                yago_level_2_body_of_water, yago_level_2_person, yago_level_3_person_writer, yago_level_3_person_artist,
-                yago_level_3_person_player, yago_level_3_person_scientist]
-    for idx, dataset_name in enumerate(dataset_names):
         if not os.path.exists(f'./dataset/{dataset_name}'):
             os.makedirs(f'./dataset/{dataset_name}')
 
-        datasets[idx].drop(columns=['Class'], inplace=True)
-        datasets[idx].to_pickle(f'./dataset/{dataset_name}/dataset.pkl')
+        subset.drop(columns=['Class'], inplace=True)
+        subset.to_pickle(f'./dataset/{dataset_name}/dataset.pkl')
+        print(f'Saved dataset {dataset_name}')
 
 
 if __name__ == '__main__':
